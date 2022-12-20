c6628c74160bd5538458c2dfe34bbf31e3daf81b
Рома Столбов <15webstolbov@gmail.com>
2022-12-20 17:42:48 +0800

    Updates

--- _nuxt/composables.aee312bb.js
+++
@@ -1,1 +0,0 @@
-import{l as r}from"./entry.74cf35b9.js";function t(e,u){return r()._useHead(e,u)}export{t as u};
---
+++ _nuxt/index.e1d7c760.js
@@ -0,0 +1,1 @@
+import{a as e,b as c,o as n}from"./entry.19f40099.js";const r={};function t(o,a){return n(),c("article",null," :) \u2014 \u0435\u0442\u043E \u044F \u0443\u043B\u044B\u0431\u0430\u044E\u0441\u044C \u0432\u0430\u043C ")}const _=e(r,[["render",t]]);export{_ as default};
