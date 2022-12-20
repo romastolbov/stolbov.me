27a6cf4fb82e98c91858bc113aea415169ca7596
Рома Столбов <15webstolbov@gmail.com>
2022-12-20 22:40:46 +0800

    feat(project): Add NotFound component

---
+++ components/NotFound/index.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="not-found">
+        <img
+            class="not-found__image"
+            src="vincent.gif"
+        >
+    </div>
+</template>
+
+<style lang="scss" scoped>
+.not-found {
+    max-width: 250px;
+    height: auto;
+}
+</style>
--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,22 @@
 <template>
-  <section>
-    <ContentDoc />
+  <section class="post-page">
+    <ContentDoc>
+        <template #not-found>
+            <NotFound class="post-page__not-found" />
+        </template>
+    </ContentDoc>
   </section>
 </template>
+
+<style lang="scss" scoped>
+.post-page {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+
+    &__not-found {
+        margin: 0 auto;
+    }
+}
+</style>
